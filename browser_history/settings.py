17b81f2b277d0433cb75e7740f7d382ed8e857f7
Brendon Welker <wel15018@byui.edu>
2019-06-19 13:47:48 -0600

    trying something

--- browser_history/settings.py
+++ browser_history/settings.py
@@ -87,7 +87,7 @@ REST_FRAMEWORK = {
     ),
 }
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
     "https://gajdbphcphelbmmcmbmokangbcleabcc.chromiumapp.org",
     "chrome-extension://gajdbphcphelbmmcmbmokangbcleabcc",
