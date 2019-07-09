53ff9ad73ae86e95370c6e0406d9b6d102167955
Brendon Welker <wel15018@byui.edu>
2019-07-09 13:09:49 -0600

    trying to allow testing on other machines

--- accounts/views.py
+++ accounts/views.py
@@ -5,11 +5,8 @@ from rest_framework.authtoken.models import Token
 from django.http import JsonResponse
 from .forms import CustomUserCreationForm
 from django.contrib import messages
-import logging
 import requests
 import json
-
-logger = logging.getLogger(__name__)
 
 
 def register(request):
@@ -18,14 +15,14 @@ def register(request):
         if form.is_valid():
             messages.success(request, "Account succesfully created! Please sign in now.")
             form.save()
-            if (request.POST.get("extension")):
-                return redirect('/extension-authentication')
+            if request.POST.get("extension"):
+                return redirect('/extension-authentication?id=' + request.POST.get("id", None))
             else :
                 return redirect('/sign-in')
     else:
         form = CustomUserCreationForm()
-        if (request.GET.get("extension")):
-            return render(request, 'accounts/create-account.html', {'form': form, 'extension': True})
+        if request.GET.get("extension"):
+            return render(request, 'accounts/create-account.html', {'form': form, 'extension': True, 'id': request.GET.get("id", None)})
         else :
             return render(request, 'accounts/create-account.html', {'form': form})
 
@@ -34,20 +31,22 @@ def extension_authentication(request):
     form = AuthenticationForm()
     if request.method == "GET":
         print("Get request")
-        return render(request, "accounts/sign-in.html", {'form': form, 'extension': True})
+        return render(request, "accounts/sign-in.html", {'form': form, 'extension': True, 'id': request.GET.get("id", None)})
     elif request.method == "POST":
         print("Post request received")
         username = request.POST.get("username", None)
         password = request.POST.get("password", None)
-        print("username: " + username)
-        print("password: " + password)
+        id = request.POST.get("id", None)
+        print("DEBUG: username: " + username)
+        print("DEBUG: password: " + password)
+        print("DEBUG: id: " + id)
         if username and password:
             data = {'username': username, 'password': password}
             print("making post request")
-            authToken = requests.post("http://localhost:8000/accounts/api-token-auth/", data).json().get("token")
-            return redirect("https://gajdbphcphelbmmcmbmokangbcleabcc.chromiumapp.org/provider_cb#authToken=" + authToken)
+            authToken = requests.post("https://daily-habbit-tracker.herokuapp.com/accounts/api-token-auth/", data).json().get("token")
+            return redirect("https://" + id + ".chromiumapp.org/provider_cb#authToken=" + authToken)
         else:
-            print("ERROR: Missing username and password")
+            print("ERROR: Missing username, password, and/or the extension id")
             return render(request, 'accounts/sign-in.html', {'form': form})
 
 
