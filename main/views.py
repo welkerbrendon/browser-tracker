70f508e047848a77f8e54b2087e8a409d250b30d
Brendon Welker <wel15018@byui.edu>
2019-07-10 14:58:50 -0600

    adding line breaks

--- main/views.py
+++ main/views.py
@@ -115,8 +115,8 @@ def get_extension_list(user, visit):
     extension_list = ""
     for extension in extensions:
         print("DEBUG: get_extension_list extension=" + str(extension))
-        extension_list += extension.page_extension + ", "
-    return extension_list[:-2]
+        extension_list += extension.page_extension + ",<br>"
+    return extension_list[:-5]
 
 
 @csrf_exempt
