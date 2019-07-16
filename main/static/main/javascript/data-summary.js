cf71ece9a5944b3d2ce3636a0dc65187d0e08944
Brendon Welker <wel15018@byui.edu>
2019-07-15 21:56:45 -0600

    adding colors

--- main/static/main/javascript/data-summary.js
+++ main/static/main/javascript/data-summary.js
@@ -1,5 +1,18 @@
 window.onload = getGraphs;
 
+const ligtBackgroundColors = [
+    rgb(144, 147, 203),
+    rgb(225, 151, 76),
+    rgb(132, 186, 91),
+    rgb(211, 94, 96),
+    rgb(128, 133, 133),
+    rgb(144, 103, 167)
+];
+
+// const darkerBackgroundColors = [
+    
+// ]
+
 function getGraphs() {
     var pieChartOverallCanvasContext = document.getElementById("pie-chart-overall").getContext("2d");
     var pieChartProductiveCanvasContext = document.getElementById("pie-chart-productive").getContext("2d");
@@ -28,7 +41,8 @@ function getGraphs() {
                     labels: Object.keys(pieChartDataJSON),
                     datasets: [ {
                         data: pieChartDataset
-                    }]
+                    }],
+                    backgroundColor: ligtBackgroundColors
                 },
                 options: {
                     title: {
