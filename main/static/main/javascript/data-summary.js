8a587bb2a82df5ffee109757f0bf1f33ed8096ef
Brendon Welker <wel15018@byui.edu>
2019-07-15 21:35:51 -0600

    working on graphs

---
+++ main/static/main/javascript/data-summary.js
@@ -0,0 +1,49 @@
+var pieChartOverallCanvasContext = document.getElementById("pie-chart-overall").getContext("2d");
+var pieChartProductiveCanvasContext = document.getElementById("pie-chart-productive").getContext("2d");
+var pieChartUnproductiveCanvasContext = document.getElementById("pie-chart-unproductive").getContext("2d");
+var barGraphCanvasContext = document.getElementById("bar-graph").getContext("2d");
+var lineGraphCanvasContext = document.getElementById("line-graph").getContext("2d");
+
+$.get("/raw-data", {}, function (result, status) {
+    if (status == "success") {
+        const pieChartDataJSON = result.pie_chart_data;
+        const barGraphDataJSON = result.bar_graph;
+        const lineGraphDataJSON = result.line_graph;
+        const productivePieChartDataJSON = result.productive_pie_chart_data;
+        const unproductivePieChartDataJSON = result.unproductive_pie_chart_data;
+
+        var pieChartDataset = getData(pieChartDataJSON);
+        var barGraphDataset = getData(barGraphDataJSON);
+        var lineGraphDataset = getData(lineGraphDataJSON);
+        var productivePieChartDataset = getData(productivePieChartDataJSON);
+        var unproductivePidChartDataset = getData(unproductivePieChartDataJSON);
+
+        var overallPieChart = new Chart(pieChartOverallCanvasContext, {
+            type: 'pie',
+            data: {
+                labels: Object.keys(pieChartDataJSON),
+                datasets: [ {
+                    data: pieChartDataset
+                }]
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: "Websites Visted"
+                },
+                legend: {
+                    display: true,
+                    position: 'right'
+                }
+            }
+        })
+    }
+});
+
+function getData(dataJSON) {
+    dataset = []
+    for (var key in dataJSON) {
+        dataset.append(dataJSON[key]);
+    }
+    return dataset;
+}
--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('activities/site/', views.site_activity, name="page-visits"),
     path('site-visits/', views.view_site_visits, name="site-visits"),
-    path('site-visits/raw-data', views.site_visit_raw_data, name="site-visits-raw"),
+    path('data-summary/raw-data', views.site_visit_raw_data, name="raw-summary"),
     path('data-summary/', views.data_summary, name="data-summary")
 ]
