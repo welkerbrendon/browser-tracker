d519b48f5c1300ac169d8f00bb44190e3bd080ed
Brendon Welker <wel15018@byui.edu>
2019-07-22 17:25:14 -0600

    another bug

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='main-home'),
     path('activities/site/', views.site_activity, name="page-visits"),
     path('site-visits/', views.view_site_visits, name="site-visits"),
     path('data-summary/raw-data', views.site_visit_raw_data, name="raw-summary"),
